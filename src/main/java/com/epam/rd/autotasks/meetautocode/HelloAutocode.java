69ca18fc702cac979729dac8e451030e2634c540
Ashish7429 <ashish.11916604@lpu.in>
2023-01-18 05:50:16 +0000

    Update file HelloAutocode.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
@@ -2,7 +2,8 @@ package com.epam.rd.autotasks.meetautocode;
 
 public class HelloAutocode {
     public static void main(String[] args) {
-        //Write a program, printing "Hello, Autocode!"
+        //Write a program, printing "Hello, Autocode!
+        System.out.println("\"Hello, Autocode!\" ");
        
     }
 }
